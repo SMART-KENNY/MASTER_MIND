f771c9d5a6d9e0e23dd6c72b240945e7daf4a1dc
HOME-Kenny <homepc_klmembrano>
2025-03-18 08:28:26 +0800

    day 2 of learning pthon Topic: Arithmetic Operators , logical operators and comparison operators

---
+++ LEARNING-PYTHON/operators/exercise_day_03.py
@@ -0,0 +1,61 @@
+
+age = int(20)
+height = float(156.7)
+complex_number = 14j
+
+
+#input_base = int(input("Enter your base: "))
+#input_height = int(input("Enter your height: "))
+
+#are_of_traingle = 0.5 * ( input_base * input_height )
+#print(f'are of triangle is: {are_of_traingle}')
+
+#left_angle_of_triangle = int(input("Enter the left angle of a triangle: "))
+#right_angle_of_triangle = int(input("Enter the right angle of a triangle: "))
+#upper_angle_of_triangle = int(input("Enter the upper angle of a triangle: "))
+
+#total_perimeter = left_angle_of_triangle + right_angle_of_triangle + upper_angle_of_triangle
+#print(f'total perimeter of a triangle is: {total_perimeter}')
+
+#rectangle_length = int(input('Enter rectangle_length: '))
+#rectangle_width = int(input('Enter rectangle_width : '))
+
+#area_of_retangle = rectangle_length * rectangle_width
+#perimeter_of_rectangle = 2 * (rectangle_length + rectangle_width)
+
+#print(f'area of rectangle: {area_of_retangle}')
+#print(f'permiter of rectangle: {perimeter_of_rectangle}')
+
+
+#radius_of_circle = int(input('enter radius: '))
+#area_of_circle = 3.1416 * (radius_of_circle ** 2) 
+
+#print(f'area of circle: {area_of_circle}')
+
+python_str = 'python'
+dragon_str = 'dragon'
+
+python_len = len(python_str)
+dragon_len = len(dragon_str)
+
+print(f'length of python: {python_len}')
+print(f'length of dragon: {dragon_len}')
+print(f'len comparison of python and dragon: {python_len == dragon_len}')
+
+print(f'is word on exist in python and dragon?: {'on' in python_str and 'on' in dragon_str}')
+
+jargon_sentence = 'I hope this course is not full of jargon'
+
+print(f'is jargon in the sentence? {jargon_sentence} , {'jargon' in jargon_sentence}')
+
+# cast
+cast_float = float(python_len)
+print(f'int to float: {cast_float}')
+
+cast_str = str(python_len)
+print(f'int to str: {cast_str}')
+
+print(f'is {python_len} even number? : {python_len % 2}')
+
+print(f'is string 10 equal to nuber 10? : {'10' == 10}')
+print(f'is string 9.8 equal to nuber 10? : {'9.8' == 10}')
---
+++ LEARNING-PYTHON/operators/notes.md
@@ -0,0 +1,33 @@
+
+### OPERATORS
+
+- Addition(+): a + b
+- Subtraction(-): a - b
+- Multiplication(*): a * b
+- Division(/): a / b
+- Modulus(%): a % b
+- Floor division(//): a // b
+- Exponentiation(**): a ** b
+
+
+### COMPARISON OPERATORS
+== equal 
+!= not equal
+> greater than
+< less than
+>= greater than or equal to
+<= less than or equal to
+
+### additional operators
+- is : Returns true if both variables are the same object(x is y)
+- is not: Returns true if both variables are not the same object(x is not y)
+- in: Returns True if the queried list contains a certain item(x in y)
+- not in: Returns True if the queried list doesn't have a certain item(x in y)
+
+
+### Logical operators
+
+- and : return True if both statements are true
+- or : returns True if one of the statements is true
+- not : reverse the result , return False if the result is true
+
